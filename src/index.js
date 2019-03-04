d4d5da67d3bcd25dfdcc44d7d3ec1957c56c2f4d
JohnPeng47 <johnpeng47@gmail.com>
2019-03-04 17:29:43 -0500

    added webconfig cnfigurations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("hello")
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App/App';
+
+// entry point for out React application
+render((
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+), document.getElementById('root'));
+
